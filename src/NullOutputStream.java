b639e201c9e8537052d84ff4caa5bdbfdc59fcd0
pycaballerom <36699772+pycaballerom@users.noreply.github.com>
2019-07-16 20:01:29 -0500

    Add files via upload

---
+++ src/NullOutputStream.java
@@ -0,0 +1,8 @@
+
+import java.io.OutputStream;
+
+class NullOutputStream extends OutputStream {
+    public void write(int b) {
+        // Do nothing
+    }
+}
---
+++ src/NullPrintStream.java
@@ -0,0 +1,8 @@
+
+import java.io.PrintStream;
+
+class NullPrintStream extends PrintStream {
+    public NullPrintStream() {
+        super(new NullOutputStream());
+    }
+}
